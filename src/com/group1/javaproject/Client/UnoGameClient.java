be56a84c044518fe0d51dde8ccd941e356c5c1fa
chuck8tenz <charleswill23@gmail.com>
2022-02-25 13:03:15 -0800

    Uno Project TEST commit -"Hello World" codded in client class -2nd try

--- src/com/group1/javaproject/Client/UnoGameClient.java
+++ src/com/group1/javaproject/Client/UnoGameClient.java
@@ -3,5 +3,6 @@ package com.group1.javaproject.Client;
 public class UnoGameClient {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world 2");
     }
 }

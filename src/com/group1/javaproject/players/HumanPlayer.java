abdb200b5b9140401dc100aac337e922e5604847
chuck8tenz <charleswill23@gmail.com>
2022-02-28 08:53:46 -0800

    first run of HumanPlayer class - skip & reverse methods on hold

--- src/com/group1/javaproject/players/HumanPlayer.java
+++ src/com/group1/javaproject/players/HumanPlayer.java
@@ -1,4 +1,46 @@
 package com.group1.javaproject.players;
 
-public class HumanPlayer {
+import com.group1.javaproject.deck.Deck;
+import com.group1.javaproject.deck.UnoCard;
+
+import java.util.List;
+
+public class HumanPlayer implements Player{
+
+    private String name;
+    private boolean isHuman = true;
+
+    public HumanPlayer(String name){
+        this.name = name;
+        playerHand.add(Deck.dealCards());
+    }
+
+    @Override
+    public void draw() {
+        playerHand.add(Deck.drawCards(1));
+    }
+
+    @Override
+    public UnoCard playCard(UnoCard card) {
+        return card;
+    }
+
+    //yell uno when hand = 1
+    @Override
+    public void sayUno() {
+        if (checkCardCount() == 1){
+            System.out.println("UNO!!!");
+        } else{
+            System.out.println("FOUL ON THE PLAY");
+        }
+    }
+
+    @Override
+    public void reverse() {
+
+    }
+    @Override
+    public void skip() {
+
+    }
 }

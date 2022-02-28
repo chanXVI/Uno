22fa0abf7c1c6b03b5742fd189ba7ff947d89183
chuck8tenz <charleswill23@gmail.com>
2022-02-28 08:53:46 -0800

    first run of HumanPlayer class - skip & reverse methods on hold

--- src/com/group1/javaproject/players/HumanPlayer.java
+++ src/com/group1/javaproject/players/HumanPlayer.java
@@ -3,11 +3,7 @@ package com.group1.javaproject.players;
 import com.group1.javaproject.deck.Deck;
 import com.group1.javaproject.deck.UnoCard;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class HumanPlayer implements Player{
 
@@ -16,28 +12,17 @@ public class HumanPlayer implements Player{
 
     public HumanPlayer(String name){
         this.name = name;
-        playerHand.addAll(Deck.dealCards());
+        playerHand.add(Deck.dealCards());
     }
 
     @Override
     public void draw() {
-        playerHand.addAll(Deck.drawCards(1));
+        playerHand.add(Deck.drawCards(1));
     }
 
     @Override
-    public UnoCard playCard() throws IOException {
-        System.out.println(playerHand);
-
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(input);
-        System.out.println("What card do you want to play?");
-        String card = reader.readLine(); //enter place in arraylist to get element
-        System.out.println("You picked:");
-        int x = Integer.parseInt(card); //entered number will be changed to int
-        System.out.println(playerHand.get(x)); //int x used to get UnoCard from "index x" of playerHand
-        playerHand.remove(x); //remove from hand
-
-        return playerHand.get(x);
+    public UnoCard playCard(UnoCard card) {
+        return card;
     }
 
     //yell uno when hand = 1
@@ -50,33 +35,12 @@ public class HumanPlayer implements Player{
         }
     }
 
-    //HOLD
     @Override
     public void reverse() {
 
     }
-
-    //HOLD
     @Override
     public void skip() {
 
-    }
-
-    @Override
-    public int checkCardCount() {
-        return Player.super.checkCardCount();
-    }
-
-    @Override
-    public boolean isCardValid(UnoCard card) {
-        return Player.super.isCardValid(card);
-    }
-
-    @Override
-    public String toString() {
-        return "HumanPlayer{" +
-                "name='" + name + '\'' +
-                ", isHuman=" + isHuman +
-                '}';
     }
 }

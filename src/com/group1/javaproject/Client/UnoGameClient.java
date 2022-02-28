984866545522eb350c98b2224f5639d537410ceb
Sanil Pannachan <spannachan@gmail.com>
2022-02-28 10:58:14 -0600

    We made version1 of the Deck and UnoCard Class.

--- src/com/group1/javaproject/Client/UnoGameClient.java
+++ src/com/group1/javaproject/Client/UnoGameClient.java
@@ -2,6 +2,6 @@ package com.group1.javaproject.Client;
 
 public class UnoGameClient {
     public static void main(String[] args) {
-        System.out.println("Trial Branch Sanil 12345");
+
     }
 }
--- src/com/group1/javaproject/players/AiPlayer.java
+++ src/com/group1/javaproject/players/AiPlayer.java
@@ -1,4 +1,5 @@
 package com.group1.javaproject.players;
 
 public class AiPlayer {
+
 }

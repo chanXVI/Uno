e1ed53dcaa36ef5fca3be28134f3035284f73ee6
chuck8tenz <charleswill23@gmail.com>
2022-02-28 12:16:41 -0800

    trying to meger HumanPlayer to main branch

--- src/com/group1/javaproject/players/HumanPlayer.java
+++ src/com/group1/javaproject/players/HumanPlayer.java
@@ -3,7 +3,11 @@ package com.group1.javaproject.players;
 import com.group1.javaproject.deck.Deck;
 import com.group1.javaproject.deck.UnoCard;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HumanPlayer implements Player{
 
@@ -12,17 +16,28 @@ public class HumanPlayer implements Player{
 
     public HumanPlayer(String name){
         this.name = name;
-        playerHand.add(Deck.dealCards());
+        playerHand.addAll(Deck.dealCards());
     }
 
     @Override
     public void draw() {
-        playerHand.add(Deck.drawCards(1));
+        playerHand.addAll(Deck.drawCards(1));
     }
 
     @Override
-    public UnoCard playCard(UnoCard card) {
-        return card;
+    public UnoCard playCard() throws IOException {
+        System.out.println(playerHand);
+
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+        System.out.println("What card do you want to play?");
+        String card = reader.readLine();
+        System.out.println("You picked:");
+        System.out.println(card);
+        int x = Integer.parseInt(card);
+        System.out.println(playerHand.get(x));
+
+        return playerHand.get(x);
     }
 
     //yell uno when hand = 1
@@ -35,12 +50,33 @@ public class HumanPlayer implements Player{
         }
     }
 
+    //HOLD
     @Override
     public void reverse() {
 
     }
+
+    //HOLD
     @Override
     public void skip() {
 
+    }
+
+    @Override
+    public int checkCardCount() {
+        return Player.super.checkCardCount();
+    }
+
+    @Override
+    public boolean isCardValid(UnoCard card) {
+        return Player.super.isCardValid(card);
+    }
+
+    @Override
+    public String toString() {
+        return "HumanPlayer{" +
+                "name='" + name + '\'' +
+                ", isHuman=" + isHuman +
+                '}';
     }
 }
--- src/com/group1/javaproject/players/Player.java
+++ src/com/group1/javaproject/players/Player.java
@@ -2,6 +2,8 @@ package com.group1.javaproject.players;
 
 import com.group1.javaproject.UnoGame.UnoGame;
 import com.group1.javaproject.deck.UnoCard;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +20,7 @@ public interface Player {
 
     void draw();
 
-    UnoCard playCard();
+    UnoCard playCard() throws IOException;
 
     void sayUno();
 
@@ -32,15 +34,14 @@ public interface Player {
 
     default boolean isCardValid(UnoCard card){
         //A more readable variable name
-        UnoCard topCard = UnoGame.topCard;
+        //UnoCard topCard = UnoGame.topCard;
 
         //If the card is a wild card, or has the same number or color as the top card, it is a valid card
-        if(card.getColor() == "Wild"  || card.getColor() == topCard.getColor() || card.getNumber() == topCard.getNumber()){
+        /*if(card.getColor() == "Wild"  || card.getColor() == topCard.getColor() || card.getNumber() == topCard.getNumber()){
             return true;
-        }
+        }*/
 
         //return false if card is not valid
         return false;
     }
-
 }

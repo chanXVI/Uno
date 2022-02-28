bd0470e6ecad31260f778247e26999902f625669
Julian <julians8522@gmail.com>
2022-02-28 09:20:15 -0800

    Implementation of starting methods in Player interface and AiPlayer class. Added Javadoc comments above methods with TODO messages

--- src/com/group1/javaproject/players/Player.java
+++ src/com/group1/javaproject/players/Player.java
@@ -1,4 +1,45 @@
 package com.group1.javaproject.players;
 
+import com.group1.javaproject.deck.UnoCard;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The player interface describes the behavior of each Player
+ *
+ * @see HumanPlayer
+ * @see AiPlayer
+ * @author Team Uno
+ */
 public interface Player {
+
+    List<UnoCard> playerHand = new ArrayList<>();
+
+    void draw();
+
+    UnoCard playCard();
+
+    void sayUno();
+
+    void reverse();
+
+    void skip();
+
+    default int checkCardCount(){
+        return playerHand.size();
+    }
+
+    default boolean isCardValid(UnoCard card){
+        //A more readable variable name
+        UnoCard topCard = UnoGame.topCard;
+
+        //If the card is a wild card, or has the same number or color as the top card, it is a valid card
+        if(card.getColor() == "Wild"  || card.getColor() == topCard.getColor() || card.getNumer() == topCard.getNumber()){
+            return true;
+        }
+
+        //return false if card is not valid
+        return false;
+    }
+
 }
